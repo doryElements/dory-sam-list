beb736665df54bcc3907198395c10cbbc76ee013
Morille Jerome <morille.jerome@groupagrica.com>
2016-11-28 16:07:44 +0100

    Travis integration

---
+++ wct.conf.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    expanded: true,
+    persistent:  false
+    // plugins: {
+        // local: { browsers: ['chrome'] },
+        // istanbul: {
+        //     dir: "./coverage",
+        //     reporters: ["text-summary", "lcov"],
+        //     include: [
+        //           "**/*.html"
+        //     ],
+        //     exclude: [
+        //         "/polymer/polymer.js",
+        //         "/platform/platform.js"
+        //     ]
+        // }
+    // }
+
+};
